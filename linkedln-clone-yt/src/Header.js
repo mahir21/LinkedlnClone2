694298813b6537aa22a77e88328f7897be2ba71e
mahir21 <mahirjeet@gmail.com>
2023-03-03 06:16:27 +0600

    applying home icons to right-header

--- linkedln-clone-yt/src/Header.js
+++ linkedln-clone-yt/src/Header.js
@@ -3,6 +3,9 @@ import './Header.css'
 
 import SearchIcon from '@mui/icons-material/Search';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HeaderOption from './HeaderOption.js';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 
 function Header() {
@@ -13,23 +16,26 @@ function Header() {
       
       <div className = "header_left">
           
-        <i className='header_left_linkedIn_Icon'>  <LinkedInIcon/> </i>
+        <i className='header_left_linkedIn_Icon'>  <LinkedInIcon fontSize="large"/> </i>
               
              
           <div className= "header_search">
                 
-                <SearchIcon/>
-                
-              <input type = "text">
+          
+                <SearchIcon />
+                <input type = "text"/>
+               
+          </div>  
                   
-              </input>
-          </div>
-      </div>
+              
+          
+        </div>
 
       <div className = "header_right">
-
-
-      </div>
+          <HeaderOption Icon = {HomeIcon} title="Home" />
+          <HeaderOption Icon = {SupervisorAccountIcon} title = "My NetWork"/> 
+          <HomeIcon/>
+       </div>
 
 
 
